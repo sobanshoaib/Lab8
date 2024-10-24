0b7532df8a51e5644fb63c7ebbe26e8b2d69e68f
Soban Shoaib <sobanshoaib7500@gmail.com>
2024-10-24 03:53:38 -0600

    new

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,12 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 
 }
